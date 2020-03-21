d72f9b65a3daae8237ebec916a9b2ce590db413c
Haley Rodzos <haley.rodzos@gmail.com>
2020-03-21 22:58:20 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -32,12 +32,11 @@
 
 def naming_system
  hoh =  { 
-   PORTION_1 =
-    :label => "Kingdom",
-      :sub_category => {
-        :label => "Phylum",
-          :sub_category => {
-            :label => "Class"
+   PORTION_1 = :label => "Kingdom",
+                  :sub_category => {
+                    :label => "Phylum",
+                       :sub_category => {
+                         :label => "Class"
     }
  }
 }
